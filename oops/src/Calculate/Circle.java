aa6c7a5288d2b4dc91a1798f8dbef8e40f7783cf
daksh1913 <165263031+daksh1913@users.noreply.github.com>
2024-07-25 10:55:18 +0530

    Add files via upload

---
+++ oops/src/Calculate/Circle.java
@@ -0,0 +1,18 @@
+package Calculate;
+
+public class Circle {
+		private double value;
+		private final float pi = 3.14f;
+		
+		public Circle(float value) {
+			super();
+			this.value = value;
+		}
+		final double getArea() {
+			double rs = value*value;
+			double output = (double)pi*rs;
+			return output;
+		}
+		
+}
+
